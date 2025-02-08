1f987e39abf81ccb9248a334b0a9a139f4a65ce0
Malavika Krishnan <krismaalu@gmail.com>
2025-02-08 21:06:29 +0530

    Update game.py

--- game.py
+++ game.py
@@ -4,13 +4,10 @@ from PIL import Image
 
 a = input('Enter the first name: ')
 b = input('Enter the second name: ')
-
 c = np.random.randint(1, 100)
 print(f'The love percentage between {a} and {b} is {c}%')
 
-
 img = Image.open(r'C:\Users\LENOVO\Downloads\\1.gif')
-
 
 values = [c, 100 - c]
 labels = [f" Love ({c}%)", "Remaining"]
@@ -21,11 +18,7 @@ ax.pie(values, labels=labels, startangle=140, colors=colors,
        wedgeprops={'edgecolor': 'Pink', 'linewidth': 2},
        textprops={'fontsize': 12, 'fontweight': 'bold', 'color': 'Purple'})
 
-
 ax.imshow(img, extent=[-2, 2, -2, 2], aspect='auto', alpha=0.3)
 
-
 plt.title(f" Love Percentage: {a} ❤ {b} ", fontsize=14, fontweight='bold', color='Pink')
-
-# Display the pie chart
 plt.show()
