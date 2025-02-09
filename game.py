c3dee47dbccf2ebf54d3aa8a74567dd9cfa57d41
Malavika Krishnan <krismaalu@gmail.com>
2025-02-09 16:12:36 +0530

    Update game.py

--- game.py
+++ game.py
@@ -10,7 +10,6 @@ img = Image.open('1.gif')
 values = [c, 100 - c]
 labels = [f" Love ({c}%)", "Remaining"]
 colors = ['Red', 'Black']  
-
 fig, ax = plt.subplots(figsize=(6, 6), facecolour='pink')
 ax.pie(values, labels=labels, startangle=140, colors=colors,
        wedgeprops={'edgecolor': 'Pink', 'linewidth': 2},
